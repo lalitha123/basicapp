52db48a19cd4af703e7c3c555298b8dbe603ada7
lalitha123 <lalitha.mycareer@gmail.com>
2022-06-07 22:07:45 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,38 @@
-var cacheName = 'helloWorld';                         
+var cacheName = 'latestNews-v1';
 
-self.addEventListener('install', event => {           
+// Cache our known resources during install
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(cacheName)                            
-      .then(cache => cache.addAll([                   
-        '/basicapp/js/script.js'
-      ]))
+    caches.open(cacheName)
+    .then(cache => cache.addAll([                               
+      '/basicapp/js/script.js'
+    ]))
   );
 });
 
-self.addEventListener('fetch', function(event) {    
+// Cache any new resources as they are fetched
+self.addEventListener('fetch', event => {                       
   event.respondWith(
-    caches.match(event.request)                     
-      .then(function(response) {
-        if (response) {                             
-          return response;                          
-        }
-        return fetch(event.request);
+    caches.match(event.request, { ignoreSearch: true })         
+    .then(function(response) {
+      if (response) {
+        return response;                                        
       }
-    )
+      var requestToCache = event.request.clone();
+
+      return fetch(requestToCache).then(                        
+        function(response) {
+          if(!response || response.status !== 200) {
+            return response;
+          }
+
+          var responseToCache = response.clone();
+          caches.open(cacheName)
+          .then(function(cache) {
+            cache.put(requestToCache, responseToCache);         
+          });
+          return response;
+        });
+    })
   );
 });
