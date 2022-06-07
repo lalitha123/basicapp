2ca1ed8bcad9e59347d66d6887f7d7b45cb7546b
lalitha123 <lalitha.mycareer@gmail.com>
2022-06-07 20:57:18 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,8 +4,7 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(cacheName)                            
       .then(cache => cache.addAll([                   
-        '/basicapp/js/script.js',
-        '/basicapp/images/hello.png'
+        '/basicapp/js/script.js'
       ]))
   );
 });
