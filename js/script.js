3f49b87f459bef9bbd78ede96a063642c678821b
lalitha123 <lalitha.mycareer@gmail.com>
2022-06-08 13:19:45 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+// Register the service worker 
+  if ('serviceWorker' in navigator) {   
+     navigator.serviceWorker.register('/basicapp/service-worker.js').then(function(registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+}).catch(function(err) {                                             
+    // registration failed :(
+    console.log('ServiceWorker registration failed: ', err);
+    });
+}
 console.log("entered");
