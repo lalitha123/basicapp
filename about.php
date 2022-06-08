6397ae48ceb0f7df68f3ebf9fa5234d759291f05
lalitha123 <lalitha.mycareer@gmail.com>
2022-06-08 13:24:43 +0530

    Create about.php

---
+++ about.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>About Us</title>
+  </head>
+  <body>
+    <h1>About Us</h1>
+    <!-- Image -->
+    <img src="/basicapp/about_us.jpg" />                            
+    <!-- JavaScript -->
+    <script async src="/basicapp/js/script.js"></script>                      
+  </body>
+</html>
